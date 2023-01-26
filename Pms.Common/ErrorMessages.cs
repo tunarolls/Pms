27cb4e977dddbdadf079e83a35945f29f3b64449
Tim Flores <tp.flores@outlook.com>
2023-01-26 20:08:25 +0800

    add error message constants

--- Pms.Common/ErrorMessages.cs
+++ Pms.Common/ErrorMessages.cs
@@ -10,8 +10,11 @@ namespace Pms.Common
     public static class ErrorMessages
     {
         public const string HrmsHttpRequestError = "HTTP Request failed. Please check your HRMS Configuration.";
+
+        // main errors
         public const string MainIsNull = "Main is not initialized.";
         public const string PayrollCodeIsNull = "Payroll code is empty.";
-        public const string SiteIsNull = "Site is empty";
+        public const string SiteIsNull = "Site is empty.";
+        public const string CompanyIsNull = "Company is empty.";
     }
 }
